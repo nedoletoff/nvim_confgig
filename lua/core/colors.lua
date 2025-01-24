4f4077d079529169f357a1d216261087d2a3684a
nedoletoff <nedoletoff@yandex.ru>
2025-01-24 23:23:06 +0300

    Init

---
+++ lua/core/colors.lua
@@ -0,0 +1,15 @@
+
+function SetColor()
+	-- Option 1
+	 vim.o.background = "dark"
+	 vim.cmd([[colorscheme gruvbox]])	
+
+	-- Option 2
+	-- vim.cmd.colorscheme "catppuccin"
+	
+	-- Option 3
+	-- vim.cmd([[colorscheme gruvbox-material]])
+	
+end
+
+SetColor()
